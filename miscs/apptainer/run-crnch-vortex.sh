28f6be3101fccf9e0f73fa19129cf41f00ed116f
Rahul Raj D N <rahul7rajdn@gmail.com>
2025-11-07 02:13:34 -0500

    Modified script to salloc crnch server

---
+++ miscs/apptainer/run-crnch-vortex.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -Jvortex-ci                              # Job name
+#SBATCH -N1 --cpus-per-task=8                	 # Number of nodes and CPUs per node required
+#SBATCH --mem-per-cpu=4G                         # Memory per core
+#SBATCH -t 02:00:00                              # Duration of the job (Ex: 2 hours)
+#SBATCH -p rg-nextgen-hpc                        # Partition Name
+#SBATCH -o /tools/ci-reports/vortex-ci-test-%j.out   # Combined output and error messages file
+#SBATCH -W                                       # Do not exit until the submitted job terminates.
+
+##Add commands here to build and execute
+cd $GITHUB_WORKSPACE
+hostname
+pwd
+ls
+cd $GITHUB_WORKSPACE/miscs/apptainer
+chmod +x run_apptainer.sh
+bash run_apptainer.sh
+
